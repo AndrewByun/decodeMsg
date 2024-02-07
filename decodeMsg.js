c11323f8a41f0bb9a1e83c0cd8b28c32654e8520
Andrew Byun <andrewbyun1@gmail.com>
2024-02-06 18:32:30 -0800

    finished decodeMsg

--- decodeMsg.js
+++ decodeMsg.js
@@ -1,256 +1,44 @@
 const fs = require('fs');
 
-
-// class TreeNode {
-//   constructor(value, data) {
-//     this.value = value;
-//     this.data = data;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
-// function lastDataInEachRowFromFile(filePath) {
-//   const result = [];
-  
-//   // Read the content of the file
-//   const data = fs.readFileSync(filePath, 'utf8');
-//   const treeData = parseTreeData(data);
-// console.log(treeData)
-//   if (!treeData || treeData.length === 0) {
-//     console.error('Invalid tree data in the file.');
-//     return result;
-//   }
-
-//   const root = buildTree(treeData);
-//   console.log(root);
-//   if (!root) {
-//     console.error('Error building the tree from the data.');
-//     return result;
-//   }
-
-//   const queue = [root];
-
-//   while (queue.length > 0) {
-//     const levelSize = queue.length;
-//     let lastData = null;
-
-//     for (let i = 0; i < levelSize; i++) {
-//       const node = queue.shift();
-
-//       lastData = node.data;
-
-//       if (node.left) {
-//         queue.push(node.left);
-//       }
-//       if (node.right) {
-//         queue.push(node.right);
-//       }
-//     }
-
-//     result.push(lastData);
-//   }
-
-//   return result;
-// }
-
-// function parseTreeData(data) {
-//   return data.trim().split('\n').map(line => {
-//     const [value, ...rest] = line.split(' ');
-//     const data = rest.join(' ');
-//     return { value: parseInt(value), data };
-//   });
-// }
-
-// function buildTree(treeData) {
-//   const sortedTree = treeData.sort((a,b)=>a.value - b.value)
-//   console.log('this is sortedTree');
-//   console.log(sortedTree)
-//   if (!sortedTree || sortedTree.length === 0) {
-//     return null;
-//   }
-
-//   const root = new TreeNode(sortedTree[0].value, sortedTree[0].data);
-
-//   for (let i = 1; i < sortedTree.length; i++) {
-//     insertNode(root, sortedTree[i].value, sortedTree[i].data);
-//   }
-
-//   return root;
-// }
-
-// function insertNode(root, value, data) {
-//   if (value < root.value) {
-//     if (root.left === null) {
-//       root.left = new TreeNode(value, data);
-//     } else {
-//       insertNode(root.left, value, data);
-//     }
-//   } else {
-//     if (root.right === null) {
-//       root.right = new TreeNode(value, data);
-//     } else {
-//       insertNode(root.right, value, data);
-//     }
-//   }
-// }
-
-
-// function insertNode(root, value, data) {
-  // Always insert to the left if there is room, otherwise insert to the right
-//   if (root.left === null) {
-//     root.left = new TreeNode(value, data);
-//   } else {
-//     root.right = new TreeNode(value, data);
-//   }
-// }
-
-
-// Example usage:
-// const filePath = './data.txt';
-
-// const result = lastDataInEachRowFromFile(filePath);
-// console.log(result);
-
-// 1
-// 23
-// 456
-
-class TreeNode {
-constructor(value, data){
-    this.root = value;
-    this.data = data;
-    this.left = null;
-    this.right = null;
-}
-}
-
-const decodeMessage = (filePath) => {
-  
-    //   // Read the content of the file
-    //   const data = fs.readFileSync(filePath, 'utf8');
-    //   const treeData = parseTreeData(data);
-    // console.log(treeData)
-    //   if (!treeData || treeData.length === 0) {
-    //     console.error('Invalid tree data in the file.');
-    //     return result;
-    //   }
-    
-    //   const root = buildTree(treeData);
-    //   console.log(root);
-    //   if (!root) {
-    //     console.error('Error building the tree from the data.');
-    //     return result;
-    //   }
-    
-    //   const queue = [root];
-    
-    //   while (queue.length > 0) {
-    //     const levelSize = queue.length;
-    //     let lastData = null;
-    
-    //     for (let i = 0; i < levelSize; i++) {
-    //       const node = queue.shift();
-    
-    //       lastData = node.data;
-    
-    //       if (node.left) {
-    //         queue.push(node.left);
-    //       }
-    //       if (node.right) {
-    //         queue.push(node.right);
-    //       }
-    //     }
-    
-    //     result.push(lastData);
-    //   }
-    
-    //   return result;
-    const result = [];
-    const data = fs.readFileSync(filePath, 'utf-8');
-    const treeData = parseData(data);
-      if (!treeData || treeData.length === 0) {
-    console.error('Invalid tree data in the file.');
-    return result;
-  }
-  
-  const root = buildTree(treeData);
-       console.log(root);
-      if (!root) {
-        console.error('Error building the tree from the data.');
-        return result;
-      }
-    
-      const queue = [root];
-
-
-  while (queue.length > 0) {
-    const levelSize = queue.length;
-    let lastData = null;
-
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-
-      lastData = node.data;
-
-      if (node.left) {
-        queue.push(node.left);
-      }
-      if (node.right) {
-        queue.push(node.right);
-      }
-    }
-
-    result.push(lastData);
-  }
-
-  return result;
-}
-
-function buildTree(treeData) {
-//   const sortedTree = treeData.sort((a,b)=>a.value - b.value)
-//   console.log('this is sortedTree');
-  console.log(treeData)
-  if (!treeData || treeData.length === 0) {
-    return null;
-  }
-
-  const root = new TreeNode(treeData[0].value, treeData[0].data);
-
-  for (let i = 1; i < treeData.length; i++) {
-    insertNode(root, treeData[i].value, treeData[i].data);
-  }
-
-  return root;
-}
-
-function insertNode(root, value, data) {
-    if (value < root.value) {
-      if (root.left === null) {
-        root.left = new TreeNode(value, data);
-      } else {
-        insertNode(root.left, value, data);
-      }
-    } else {
-      if (root.right === null) {
-        root.right = new TreeNode(value, data);
-      } else {
-        insertNode(root.right, value, data);
+function decode(messageFile) {
+    //read the passed in file using readFileSync, trim the white space, split into an array by new lines, and filter out any empty lines
+  const lines = fs
+    .readFileSync(messageFile, 'utf-8')
+    .trim()
+    .split('\n')
+    .filter((line) => line !== '');
+    //store the length of the array in a var
+  const numberOfItems = lines.length;
+  // Initialize an array to store the message words
+  const messageWords = new Array(100).fill('');
+  // Loop through the lines
+  for (const line of lines) {
+    // Split the line by space and get the first element as the number
+    const number = parseInt(line.split(' ')[0]);
+    // Determine if the number equates to the last element value of a "pyramid" line
+    for (let x = 0; x < numberOfItems; x++) {
+      if (number === parseInt((x * x + x) / 2)) {
+        // Grab the second element after splitting the line, which is the word
+        messageWords[x] = line.split(' ')[1];
+        //break from the loop since we have found the matching position, and move on to the next line
+        break;
       }
     }
   }
-
-
-const parseData = (data) => {
-  return data.trim().split('\n').map(line => {
-    const [value, ...rest] = line.split(' ');
-    const data = rest.join(' ');
-    return { value: parseInt(value), data };
-  });
-
+  // Now, concatenate the non-null array values together to form a sentence.
+  const text = messageWords.filter((word) => word !== '').join(' ');
+  // Return the result
+  return text;
 }
 
-const filePath = './data.txt';
+const filePath = 'data.txt';
+const decodedMessage = decode(filePath);
+console.log(decodedMessage);
 
-const result = decodeMessage(filePath);
-console.log(result);
+
+
+// BRIEF: To decode the message from the file, I created a function that reads and parses the file, iterates over the array, tracks the values of the items that have a number that corresponds to the end of a pyramid row, and eventually returns the strings that belonged to items that were at the end of rows. 
+
+// To keep track of the output, I created an array with the same length as the data array but with null values. As I looped over the data array, I updated the the result array at the same index but with the string value. 
+
+// To determine if the number belonged to the end of a pyramid row, I used a common arithmetic function 
